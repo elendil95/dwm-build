881d3ca7529e8fa38c0a2231fffd7d1029e8e5e6
Elendil95 <uge1995@gmail.com>
2020-06-06 16:58:42 +0200

    restored dmenu and added more programs

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -60,14 +60,18 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "Run: ", "-fn", dmenufont, "-nb" "#000000" "-nf" "#fefefe", NULL };
-/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "Run: ", "-fn", dmenufont, "-nb" "#000000" "-nf" "#fefefe", NULL }; */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 static const char *passcmd[]  = { "keepass", NULL };
 static const char *emailcmd[]  = { "thunderbird", NULL };
-static const char *filemanagercmd[]  = { "urxvt -e ranger", NULL };
+static const char *filemanagercmd[]  = { "urxvt", "-e", "ranger", NULL };
+static const char *musicplayercmd[]  = { "urxvt", "-e", "cmus", NULL };
+static const char *calendarcmd[]  = { "urxvt", "-e", "calcurse", NULL };
 
+//System Scripts
+static const char *sessmngrscript[]  = { "dmenu_session_manager", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -81,9 +85,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, // make Master bigger
 	{ MODKEY|ShiftMask,             XK_space,  zoom,           {0} }, //Set focused window as Master
 	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_Tab,    setlayout,      {0} }, //toggle between layouts
-	{ MODKEY|ShiftMask,             XK_t,      togglefloating, {0} },
+	{ MODKEY,                       XK_w,      killclient,     {0} }, // Close focused window
+	{ MODKEY,                       XK_Tab,    setlayout,      {0} }, // toggle between layouts
+	{ MODKEY|ShiftMask,             XK_t,      togglefloating, {0} }, // Toggle floating
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -100,6 +104,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	{ MODKEY|ControlMask,             XK_q,      quit,           {0} },
 	{ MODKEY,             XK_b, spawn,          {.v = browsercmd } },
+	{ MODKEY,             XK_p, spawn,          {.v = passcmd } },
+	{ MODKEY,             XK_e, spawn,          {.v = emailcmd } },
+	{ MODKEY,             XK_f, spawn,          {.v = filemanagercmd } },
+	{ MODKEY,             XK_m, spawn,          {.v = musicplayercmd } },
+	{ MODKEY,             XK_c, spawn,          {.v = calendarcmd } },
+	{ MODKEY|ControlMask,             XK_r,      spawn,           {.v = sessmngrscript } },
 };
 
 /* button definitions */
